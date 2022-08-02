64896a00eee14039d8d77f230be978d98bd24459
gan_74 <gregoire.angerand@gmail.com>
2022-08-02 20:53:06 +0200

    Added basic windowing

--- TP/src/main.cpp
+++ TP/src/main.cpp
@@ -1,6 +1,45 @@
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
+void glfw_check(bool cond) {
+    if(!cond) {
+        const char* err = nullptr;
+        glfwGetError(&err);
+        std::cerr << "GLFW error: " << err << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+void render_gl(GLFWwindow* window) {
+    glfwMakeContextCurrent(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main(int, char**) {
-    std::cout << "hello 3D" << std::endl;
+    glfw_check(glfwInit());
+    GLFWwindow* window = glfwCreateWindow(640, 480, "TP window", nullptr, nullptr);
+    glfw_check(window);
+
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+
+    glClearColor(0.5f, 0.7f, 0.8f, 0.0f);
+
+    for(;;) {
+        glfwPollEvents();
+        if(glfwWindowShouldClose(window) || glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+            break;
+        }
+
+        render_gl(window);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
