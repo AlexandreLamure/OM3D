0f2795b3d21f0b66bc3a0948873ce934e639f8b6
Grégoire Angerand <gregoire.angerand@gmail.com>
2022-08-13 20:12:23 +0200

    More uniforms

--- TP/src/Program.cpp
+++ TP/src/Program.cpp
@@ -93,13 +93,52 @@ Program Program::from_files(const std::string& frag, const std::string& vert) {
     return Program(read_shader(frag), read_shader(vert));
 }
 
+int Program::find_location(u32 hash) {
+    const auto it = std::lower_bound(_uniform_locations.begin(), _uniform_locations.end(), UniformLocationInfo{hash, 0});
+    return (it == _uniform_locations.end() || it->name_hash != hash) ? -1 : it->location;
+}
+
+
+
+
 void Program::set_uniform(u32 name_hash, float value) {
     if(const int loc = find_location(name_hash); loc >= 0) {
         glProgramUniform1f(_handle.get(), loc, value);
     }
 }
 
-int Program::find_location(u32 hash) {
-    const auto it = std::lower_bound(_uniform_locations.begin(), _uniform_locations.end(), UniformLocationInfo{hash, 0});
-    return it == _uniform_locations.end() ? -1 : it->location;
+void Program::set_uniform(u32 name_hash, glm::vec2 value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniform2f(_handle.get(), loc, value.x, value.y);
+    }
+}
+
+void Program::set_uniform(u32 name_hash, glm::vec3 value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniform3f(_handle.get(), loc, value.x, value.y, value.z);
+    }
+}
+
+void Program::set_uniform(u32 name_hash, glm::vec4 value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniform4f(_handle.get(), loc, value.x, value.y, value.z, value.w);
+    }
+}
+
+void Program::set_uniform(u32 name_hash, const glm::mat2& value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniformMatrix2fv(_handle.get(), loc, 1, false, reinterpret_cast<const float*>(&value));
+    }
+}
+
+void Program::set_uniform(u32 name_hash, const glm::mat3& value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniformMatrix3fv(_handle.get(), loc, 1, false, reinterpret_cast<const float*>(&value));
+    }
+}
+
+void Program::set_uniform(u32 name_hash, const glm::mat4& value) {
+    if(const int loc = find_location(name_hash); loc >= 0) {
+        glProgramUniformMatrix4fv(_handle.get(), loc, 1, false, reinterpret_cast<const float*>(&value));
+    }
 }
--- TP/src/Program.h
+++ TP/src/Program.h
@@ -3,6 +3,13 @@
 
 #include <graphics.h>
 
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat2x2.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <vector>
 
 class Program : NonCopyable {
@@ -32,6 +39,12 @@ class Program : NonCopyable {
         static Program from_files(const std::string& frag, const std::string& vert);
 
         void set_uniform(u32 name_hash, float value);
+        void set_uniform(u32 name_hash, glm::vec2 value);
+        void set_uniform(u32 name_hash, glm::vec3 value);
+        void set_uniform(u32 name_hash, glm::vec4 value);
+        void set_uniform(u32 name_hash, const glm::mat2& value);
+        void set_uniform(u32 name_hash, const glm::mat3& value);
+        void set_uniform(u32 name_hash, const glm::mat4& value);
 
         template<typename T>
         void set_uniform(std::string_view name, const T& value) {
