26dd3b2a2655dc49fa0253e94a5d358cbab598da
kerian.allaire <kerian.allaire@epita.fr>
2024-11-14 13:07:52 +0100

    feat(Culling): formating and backface culling

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -3,99 +3,115 @@
 #include <glad/gl.h>
 
 #define STB_IMAGE_IMPLEMENTATION
+#include <algorithm>
+#include <cmath>
 #include <stb/stb_image.h>
 
-#include <cmath>
-#include <algorithm>
+namespace OM3D
+{
 
-namespace OM3D {
+    Result<TextureData> TextureData::from_file(const std::string& file)
+    {
+        int width = 0;
+        int height = 0;
+        int channels = 0;
+        u8* img = stbi_load(file.c_str(), &width, &height, &channels, 4);
+        DEFER(stbi_image_free(img));
+        if (!img || width <= 0 || height <= 0 || channels <= 0)
+        {
+            return { false, {} };
+        }
 
-Result<TextureData> TextureData::from_file(const std::string& file) {
-    int width = 0;
-    int height = 0;
-    int channels = 0;
-    u8* img = stbi_load(file.c_str(), &width, &height, &channels, 4);
-    DEFER(stbi_image_free(img));
-    if(!img || width <= 0 || height <= 0 || channels <= 0) {
-        return {false, {}};
+        const size_t bytes = width * height * 4;
+
+        TextureData data;
+        data.size = glm::uvec2(width, height);
+        data.format = ImageFormat::RGBA8_UNORM;
+        data.data = std::make_unique<u8[]>(bytes);
+        std::copy_n(img, bytes, data.data.get());
+
+        return { true, std::move(data) };
     }
 
-    const size_t bytes = width * height * 4;
-
-    TextureData data;
-    data.size = glm::uvec2(width, height);
-    data.format = ImageFormat::RGBA8_UNORM;
-    data.data = std::make_unique<u8[]>(bytes);
-    std::copy_n(img, bytes, data.data.get());
-
-    return {true, std::move(data)};
-}
-
-
-
-static GLuint create_texture_handle() {
-    GLuint handle = 0;
-    glCreateTextures(GL_TEXTURE_2D, 1, &handle);
-    return handle;
-}
-
-Texture::Texture(const TextureData& data) :
-    _handle(create_texture_handle()),
-    _size(data.size),
-    _format(data.format) {
-
-    const ImageFormatGL gl_format = image_format_to_gl(_format);
-    glTextureStorage2D(_handle.get(), mip_levels(_size), gl_format.internal_format, _size.x, _size.y);
-    glTextureSubImage2D(_handle.get(), 0, 0, 0, _size.x, _size.y, gl_format.format, gl_format.component_type, data.data.get());
-
-    if(bindless_enabled()) {
-        _bindless = glGetTextureHandleARB(_handle.get());
-        glMakeTextureHandleResidentARB(_bindless);
+    static GLuint create_texture_handle()
+    {
+        GLuint handle = 0;
+        glCreateTextures(GL_TEXTURE_2D, 1, &handle);
+        return handle;
     }
 
-    //glGenerateTextureMipmap(_handle.get());
-}
+    Texture::Texture(const TextureData& data)
+        : _handle(create_texture_handle())
+        , _size(data.size)
+        , _format(data.format)
+    {
+        const ImageFormatGL gl_format = image_format_to_gl(_format);
+        glTextureStorage2D(_handle.get(), mip_levels(_size),
+                           gl_format.internal_format, _size.x, _size.y);
+        glTextureSubImage2D(_handle.get(), 0, 0, 0, _size.x, _size.y,
+                            gl_format.format, gl_format.component_type,
+                            data.data.get());
 
-Texture::Texture(const glm::uvec2 &size, ImageFormat format) :
-    _handle(create_texture_handle()),
-    _size(size),
-    _format(format) {
+        if (bindless_enabled())
+        {
+            _bindless = glGetTextureHandleARB(_handle.get());
+            glMakeTextureHandleResidentARB(_bindless);
+        }
 
-    const ImageFormatGL gl_format = image_format_to_gl(_format);
-    glTextureStorage2D(_handle.get(), 1, gl_format.internal_format, _size.x, _size.y);
-
-    if(bindless_enabled()) {
-        _bindless = glGetTextureHandleARB(_handle.get());
-        glMakeTextureHandleResidentARB(_bindless);
+        glGenerateTextureMipmap(_handle.get());
     }
-}
 
-Texture::~Texture() {
-    if(auto handle = _handle.get()) {
-        glDeleteTextures(1, &handle);
+    Texture::Texture(const glm::uvec2& size, ImageFormat format)
+        : _handle(create_texture_handle())
+        , _size(size)
+        , _format(format)
+    {
+        const ImageFormatGL gl_format = image_format_to_gl(_format);
+        glTextureStorage2D(_handle.get(), 1, gl_format.internal_format, _size.x,
+                           _size.y);
+
+        if (bindless_enabled())
+        {
+            _bindless = glGetTextureHandleARB(_handle.get());
+            glMakeTextureHandleResidentARB(_bindless);
+        }
     }
-}
 
-void Texture::bind(u32 index) const {
-    glBindTextureUnit(index, _handle.get());
-}
+    Texture::~Texture()
+    {
+        if (auto handle = _handle.get())
+        {
+            glDeleteTextures(1, &handle);
+        }
+    }
 
-void Texture::bind_as_image(u32 index, AccessType access) {
-    glBindImageTexture(index, _handle.get(), 0, false, 0, access_type_to_gl(access), image_format_to_gl(_format).internal_format);
-}
+    void Texture::bind(u32 index) const
+    {
+        glBindTextureUnit(index, _handle.get());
+    }
 
-u64 Texture::bindless_handle() const {
-    return _bindless;
-}
+    void Texture::bind_as_image(u32 index, AccessType access)
+    {
+        glBindImageTexture(index, _handle.get(), 0, false, 0,
+                           access_type_to_gl(access),
+                           image_format_to_gl(_format).internal_format);
+    }
 
-glm::uvec2 Texture::size() const {
-    return _size;
-}
+    u64 Texture::bindless_handle() const
+    {
+        return _bindless;
+    }
 
-// Return number of mip levels needed
-u32 Texture::mip_levels(glm::uvec2 size) {
-    const float side = float(std::max(size.x, size.y));
-    return 1 + u32(std::floor(std::log2(side)));
-}
+    glm::uvec2 Texture::size() const
+    {
+        return _size;
+    }
 
-}
+    // Return number of mip levels needed
+    u32 Texture::mip_levels(glm::uvec2 size)
+    {
+        const float side = float(std::max(size.x, size.y));
+        return 1 + u32(std::floor(std::log2(side)));
+    }
+
+} // namespace OM3D
--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,51 @@
 #include <glad/gl.h>
 
 #define GLFW_INCLUDE_NONE
+#include <Framebuffer.h>
 #include <GLFW/glfw3.h>
-
-#include <graphics.h>
+#include <ImGuiRenderer.h>
 #include <Scene.h>
 #include <Texture.h>
-#include <Framebuffer.h>
 #include <TimestampQuery.h>
-#include <ImGuiRenderer.h>
-
+#include <filesystem>
+#include <graphics.h>
 #include <imgui/imgui.h>
-
 #include <iostream>
 #include <vector>
-#include <filesystem>
 
 using namespace OM3D;
-
 
 static float delta_time = 0.0f;
 static std::unique_ptr<Scene> scene;
 static float exposure = 1.0;
 static std::vector<std::string> scene_files;
 
-namespace OM3D {
-extern bool audit_bindings_before_draw;
+namespace OM3D
+{
+    extern bool audit_bindings_before_draw;
 }
 
-void parse_args(int argc, char** argv) {
-    for(int i = 1; i < argc; ++i) {
+void parse_args(int argc, char** argv)
+{
+    for (int i = 1; i < argc; ++i)
+    {
         const std::string_view arg = argv[i];
 
-        if(arg == "--validate") {
+        if (arg == "--validate")
+        {
             OM3D::audit_bindings_before_draw = true;
-        } else {
+        }
+        else
+        {
             std::cerr << "Unknown argument \"" << arg << "\"" << std::endl;
         }
     }
 }
 
-void glfw_check(bool cond) {
-    if(!cond) {
+void glfw_check(bool cond)
+{
+    if (!cond)
+    {
         const char* err = nullptr;
         glfwGetError(&err);
         std::cerr << "GLFW error: " << err << std::endl;
@@ -50,14 +54,16 @@ void glfw_check(bool cond) {
     }
 }
 
-void update_delta_time() {
+void update_delta_time()
+{
     static double time = 0.0;
     const double new_time = program_time();
     delta_time = float(new_time - time);
     time = new_time;
 }
 
-void process_inputs(GLFWwindow* window, Camera& camera) {
+void process_inputs(GLFWwindow* window, Camera& camera)
+{
     static glm::dvec2 mouse_pos;
 
     glm::dvec2 new_mouse_pos;
@@ -65,36 +71,50 @@ void process_inputs(GLFWwindow* window, Camera& camera) {
 
     {
         glm::vec3 movement = {};
-        if(glfwGetKey(window, 'W') == GLFW_PRESS) {
+        if (glfwGetKey(window, 'W') == GLFW_PRESS)
+        {
             movement += camera.forward();
         }
-        if(glfwGetKey(window, 'S') == GLFW_PRESS) {
+        if (glfwGetKey(window, 'S') == GLFW_PRESS)
+        {
             movement -= camera.forward();
         }
-        if(glfwGetKey(window, 'D') == GLFW_PRESS) {
+        if (glfwGetKey(window, 'D') == GLFW_PRESS)
+        {
             movement += camera.right();
         }
-        if(glfwGetKey(window, 'A') == GLFW_PRESS) {
+        if (glfwGetKey(window, 'A') == GLFW_PRESS)
+        {
             movement -= camera.right();
         }
 
         float speed = 10.0f;
-        if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        {
             speed *= 10.0f;
         }
 
-        if(movement.length() > 0.0f) {
-            const glm::vec3 new_pos = camera.position() + movement * delta_time * speed;
-            camera.set_view(glm::lookAt(new_pos, new_pos + camera.forward(), camera.up()));
+        if (movement.length() > 0.0f)
+        {
+            const glm::vec3 new_pos =
+                camera.position() + movement * delta_time * speed;
+            camera.set_view(
+                glm::lookAt(new_pos, new_pos + camera.forward(), camera.up()));
         }
     }
 
-    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
         const glm::vec2 delta = glm::vec2(mouse_pos - new_mouse_pos) * 0.01f;
-        if(delta.length() > 0.0f) {
-            glm::mat4 rot = glm::rotate(glm::mat4(1.0f), delta.x, glm::vec3(0.0f, 1.0f, 0.0f));
+        if (delta.length() > 0.0f)
+        {
+            glm::mat4 rot = glm::rotate(glm::mat4(1.0f), delta.x,
+                                        glm::vec3(0.0f, 1.0f, 0.0f));
             rot = glm::rotate(rot, delta.y, camera.right());
-            camera.set_view(glm::lookAt(camera.position(), camera.position() + (glm::mat3(rot) * camera.forward()), (glm::mat3(rot) * camera.up())));
+            camera.set_view(glm::lookAt(
+                camera.position(),
+                camera.position() + (glm::mat3(rot) * camera.forward()),
+                (glm::mat3(rot) * camera.up())));
         }
     }
 
@@ -108,7 +128,8 @@ void process_inputs(GLFWwindow* window, Camera& camera) {
     mouse_pos = new_mouse_pos;
 }
 
-void gui(ImGuiRenderer& imgui) {
+void gui(ImGuiRenderer& imgui)
+{
     const ImVec4 error_text_color = ImVec4(1.0f, 0.3f, 0.3f, 1.0f);
     const ImVec4 warning_text_color = ImVec4(1.0f, 0.8f, 0.4f, 1.0f);
 
@@ -122,34 +143,43 @@ void gui(ImGuiRenderer& imgui) {
     // ImGui::ShowDemoWindow();
 
     bool open_scene_popup = false;
-    if(ImGui::BeginMainMenuBar()) {
-        if(ImGui::BeginMenu("File")) {
-            if(ImGui::MenuItem("Open Scene")) {
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Open Scene"))
+            {
                 open_scene_popup = true;
             }
             ImGui::EndMenu();
         }
 
-        if(ImGui::BeginMenu("Exposure")) {
-            ImGui::DragFloat("Exposure", &exposure, 0.25f, 0.01f, 100.0f, "%.2f", ImGuiSliderFlags_Logarithmic);
-            if(exposure != 1.0f && ImGui::Button("Reset")) {
+        if (ImGui::BeginMenu("Exposure"))
+        {
+            ImGui::DragFloat("Exposure", &exposure, 0.25f, 0.01f, 100.0f,
+                             "%.2f", ImGuiSliderFlags_Logarithmic);
+            if (exposure != 1.0f && ImGui::Button("Reset"))
+            {
                 exposure = 1.0f;
             }
             ImGui::EndMenu();
         }
 
-        if(scene && ImGui::BeginMenu("Scene Info")) {
+        if (scene && ImGui::BeginMenu("Scene Info"))
+        {
             ImGui::Text("%u objects", u32(scene->objects().size()));
             ImGui::Text("%u point lights", u32(scene->point_lights().size()));
             ImGui::EndMenu();
         }
 
-        if(ImGui::MenuItem("GPU Profiler")) {
+        if (ImGui::MenuItem("GPU Profiler"))
+        {
             open_gpu_profiler = true;
         }
 
         ImGui::Separator();
-        ImGui::TextUnformatted(reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
+        ImGui::TextUnformatted(
+            reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
 
         ImGui::Separator();
         ImGui::Text("%.2f ms", delta_time * 1000.0f);
@@ -159,47 +189,65 @@ void gui(ImGuiRenderer& imgui) {
         ImGui::TextColored(warning_text_color, ICON_FA_BUG " (DEBUG)");
 #endif
 
-        if(!bindless_enabled()) {
+        if (!bindless_enabled())
+        {
             ImGui::Separator();
-            ImGui::TextColored(error_text_color, ICON_FA_EXCLAMATION_TRIANGLE " Bindless textures not supported");
+            ImGui::TextColored(error_text_color,
+                               ICON_FA_EXCLAMATION_TRIANGLE
+                               " Bindless textures not supported");
         }
         ImGui::EndMainMenuBar();
     }
 
-    if(open_scene_popup) {
+    if (open_scene_popup)
+    {
         ImGui::OpenPopup("###openscenepopup");
 
         scene_files.clear();
-        for(auto&& entry : std::filesystem::directory_iterator(data_path)) {
-            if(entry.status().type() == std::filesystem::file_type::regular) {
+        for (auto&& entry : std::filesystem::directory_iterator(data_path))
+        {
+            if (entry.status().type() == std::filesystem::file_type::regular)
+            {
                 const auto ext = entry.path().extension();
-                if(ext == ".gltf" || ext == ".glb") {
+                if (ext == ".gltf" || ext == ".glb")
+                {
                     scene_files.emplace_back(entry.path().string());
                 }
             }
         }
     }
 
-    if(ImGui::BeginPopup("###openscenepopup", ImGuiWindowFlags_AlwaysAutoResize)) {
+    if (ImGui::BeginPopup("###openscenepopup",
+                          ImGuiWindowFlags_AlwaysAutoResize))
+    {
         auto load_scene = [](const std::string path) {
             auto result = Scene::from_gltf(path);
-            if(!result.is_ok) {
-                std::cerr << "Unable to load scene (" << path << ")" << std::endl;
-            } else {
+            if (!result.is_ok)
+            {
+                std::cerr << "Unable to load scene (" << path << ")"
+                          << std::endl;
+            }
+            else
+            {
                 scene = std::move(result.value);
             }
             ImGui::CloseCurrentPopup();
         };
 
         char buffer[1024] = {};
-        if(ImGui::InputText("Load scene", buffer, sizeof(buffer), ImGuiInputTextFlags_EnterReturnsTrue)) {
+        if (ImGui::InputText("Load scene", buffer, sizeof(buffer),
+                             ImGuiInputTextFlags_EnterReturnsTrue))
+        {
             load_scene(buffer);
         }
 
-        if(!scene_files.empty()) {
-            for(const std::string& p : scene_files) {
+        if (!scene_files.empty())
+        {
+            for (const std::string& p : scene_files)
+            {
                 const auto abs = std::filesystem::absolute(p).string();
-                if(ImGui::MenuItem(abs.c_str())) {
+                if (ImGui::MenuItem(abs.c_str()))
+                {
                     load_scene(p);
                     break;
                 }
@@ -209,29 +257,35 @@ void gui(ImGuiRenderer& imgui) {
         ImGui::EndPopup();
     }
 
-    if(open_gpu_profiler) {
-        if(ImGui::Begin(ICON_FA_CLOCK " GPU Profiler")) {
-            const ImGuiTableFlags table_flags =
-                ImGuiTableFlags_SortTristate |
-                ImGuiTableFlags_NoSavedSettings |
-                ImGuiTableFlags_SizingFixedFit |
-                ImGuiTableFlags_BordersInnerV |
-                ImGuiTableFlags_Resizable |
-                ImGuiTableFlags_RowBg;
+    if (open_gpu_profiler)
+    {
+        if (ImGui::Begin(ICON_FA_CLOCK " GPU Profiler"))
+        {
+            const ImGuiTableFlags table_flags = ImGuiTableFlags_SortTristate
+                | ImGuiTableFlags_NoSavedSettings
+                | ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_BordersInnerV
+                | ImGuiTableFlags_Resizable | ImGuiTableFlags_RowBg;
 
-            ImGui::PushStyleColor(ImGuiCol_TableRowBgAlt, ImVec4(1, 1, 1, 0.01f));
+            ImGui::PushStyleColor(ImGuiCol_TableRowBgAlt,
+                                  ImVec4(1, 1, 1, 0.01f));
             DEFER(ImGui::PopStyleColor());
 
-            if(ImGui::BeginTable("##timetable", 3, table_flags)) {
-                ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthStretch);
-                ImGui::TableSetupColumn("CPU (ms)", ImGuiTableColumnFlags_NoResize, 70.0f);
-                ImGui::TableSetupColumn("GPU (ms)", ImGuiTableColumnFlags_NoResize, 70.0f);
+            if (ImGui::BeginTable("##timetable", 3, table_flags))
+            {
+                ImGui::TableSetupColumn("Name",
+                                        ImGuiTableColumnFlags_WidthStretch);
+                ImGui::TableSetupColumn("CPU (ms)",
+                                        ImGuiTableColumnFlags_NoResize, 70.0f);
+                ImGui::TableSetupColumn("GPU (ms)",
+                                        ImGuiTableColumnFlags_NoResize, 70.0f);
                 ImGui::TableHeadersRow();
 
                 std::vector<u32> indents;
-                for(const auto& zone : retrieve_profile()) {
+                for (const auto& zone : retrieve_profile())
+                {
                     auto color_from_time = [](float time) {
-                        const float t = std::min(time / 0.008f, 1.0f); // 8ms = red
+                        const float t =
+                            std::min(time / 0.008f, 1.0f); // 8ms = red
                         return ImVec4(t, 1.0f - t, 0.0f, 1.0f);
                     };
 
@@ -240,21 +294,25 @@ void gui(ImGuiRenderer& imgui) {
                     ImGui::TextUnformatted(zone.name.data());
 
                     ImGui::TableSetColumnIndex(1);
-                    ImGui::PushStyleColor(ImGuiCol_Text, color_from_time(zone.cpu_time));
+                    ImGui::PushStyleColor(ImGuiCol_Text,
+                                          color_from_time(zone.cpu_time));
                     ImGui::Text("%.2f", zone.cpu_time * 1000.0f);
 
                     ImGui::TableSetColumnIndex(2);
-                    ImGui::PushStyleColor(ImGuiCol_Text, color_from_time(zone.gpu_time));
+                    ImGui::PushStyleColor(ImGuiCol_Text,
+                                          color_from_time(zone.gpu_time));
                     ImGui::Text("%.2f", zone.gpu_time * 1000.0f);
 
                     ImGui::PopStyleColor(2);
 
-                    if(!indents.empty() && --indents.back() == 0) {
+                    if (!indents.empty() && --indents.back() == 0)
+                    {
                         indents.pop_back();
                         ImGui::Unindent();
                     }
 
-                    if(zone.contained_zones) {
+                    if (zone.contained_zones)
+                    {
                         indents.push_back(zone.contained_zones);
                         ImGui::Indent();
                     }
@@ -267,10 +325,8 @@ void gui(ImGuiRenderer& imgui) {
     }
 }
 
-
-
-
-std::unique_ptr<Scene> create_default_scene() {
+std::unique_ptr<Scene> create_default_scene()
+{
     auto scene = std::make_unique<Scene>();
 
     // Load default cube model
@@ -299,18 +355,23 @@ std::unique_ptr<Scene> create_default_scene() {
     return scene;
 }
 
-struct RendererState {
-    static RendererState create(glm::uvec2 size) {
+struct RendererState
+{
+    static RendererState create(glm::uvec2 size)
+    {
         RendererState state;
 
         state.size = size;
 
-        if(state.size.x > 0 && state.size.y > 0) {
+        if (state.size.x > 0 && state.size.y > 0)
+        {
             state.depth_texture = Texture(size, ImageFormat::Depth32_FLOAT);
             state.lit_hdr_texture = Texture(size, ImageFormat::RGBA16_FLOAT);
             state.tone_mapped_texture = Texture(size, ImageFormat::RGBA8_UNORM);
-            state.main_framebuffer = Framebuffer(&state.depth_texture, std::array{&state.lit_hdr_texture});
-            state.tone_map_framebuffer = Framebuffer(nullptr, std::array{&state.tone_mapped_texture});
+            state.main_framebuffer = Framebuffer(
+                &state.depth_texture, std::array{ &state.lit_hdr_texture });
+            state.tone_map_framebuffer =
+                Framebuffer(nullptr, std::array{ &state.tone_mapped_texture });
         }
 
         return state;
@@ -326,12 +387,12 @@ struct RendererState {
     Framebuffer tone_map_framebuffer;
 };
 
-
-
-
-
-int main(int argc, char** argv) {
-    DEBUG_ASSERT([] { std::cout << "Debug asserts enabled" << std::endl; return true; }());
+int main(int argc, char** argv)
+{
+    DEBUG_ASSERT([] {
+        std::cout << "Debug asserts enabled" << std::endl;
+        return true;
+    }());
 
     parse_args(argc, argv);
 
@@ -341,7 +402,6 @@ int main(int argc, char** argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     GLFWwindow* window = glfwCreateWindow(1600, 900, "OM3D", nullptr, nullptr);
     glfw_check(window);
@@ -358,9 +418,12 @@ int main(int argc, char** argv) {
     auto tonemap_program = Program::from_files("tonemap.frag", "screen.vert");
     RendererState renderer;
 
-    for(;;) {
+    for (;;)
+    {
         glfwPollEvents();
-        if(glfwWindowShouldClose(window) || glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+        if (glfwWindowShouldClose(window)
+            || glfwGetKey(window, GLFW_KEY_ESCAPE))
+        {
             break;
         }
 
@@ -371,14 +434,17 @@ int main(int argc, char** argv) {
             int height = 0;
             glfwGetWindowSize(window, &width, &height);
 
-            if(renderer.size != glm::uvec2(width, height)) {
+            if (renderer.size != glm::uvec2(width, height))
+            {
                 renderer = RendererState::create(glm::uvec2(width, height));
             }
         }
 
         update_delta_time();
 
-        if(const auto& io = ImGui::GetIO(); !io.WantCaptureMouse && !io.WantCaptureKeyboard) {
+        if (const auto& io = ImGui::GetIO();
+            !io.WantCaptureMouse && !io.WantCaptureKeyboard)
+        {
             process_inputs(window, scene->camera());
         }
 
@@ -398,6 +464,7 @@ int main(int argc, char** argv) {
             {
                 PROFILE_GPU("Tonemap");
 
+                glDisable(GL_CULL_FACE);
                 renderer.tone_map_framebuffer.bind(false, true);
                 tonemap_program->bind();
                 tonemap_program->set_uniform(HASH("exposure"), exposure);
